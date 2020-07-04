cf23de897875ae402e61ac0f37703cd0b9b2464e
Jana Kumar <janarthanan.kumar@outlook.com>
2020-07-04 15:54:44 -0400

    add netconf filter

--- IOS-XE/netconf-3.py
+++ IOS-XE/netconf-3.py
@@ -5,7 +5,7 @@ netconf_filter = """
 <filter>
  <
 
-
+"""
 with manager.connect(host="ios-xe-mgmt-latest.cisco.com", port=10000, username="developer", password="C1sco12345", hostkey_verify=False) as m:
     for capability in m.server_capabilities:
         print('*' * 50)
