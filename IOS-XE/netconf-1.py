c66011ba71f7257caae9963d8b2667de1080d3e8
Jana Kumar <janarthanan.kumar@outlook.com>
2020-07-04 15:44:08 -0400

    na

--- IOS-XE/netconf-1.py
+++ IOS-XE/netconf-1.py
@@ -1,4 +1,4 @@
 from ncclient import manager
-
+#change
 with manager.connect(host="ios-xe-mgmt-latest.cisco.com", port=10000, username="developer", password="C1sco12345", hostkey_verify=False) as m:
     m.close_session()
